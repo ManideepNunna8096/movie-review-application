af7edcde087e4cab77df7f75118dcc4fab6c6d16
manideepnunna <nunnamanideep.study24@gmail.com>
2026-09-05 15:01:08 +0530

    Day 1: Start Movie Review Application

---
+++ src/main/java/nunna/manideep/movies/Review.java
@@ -0,0 +1,19 @@
+package nunna.manideep.movies;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Review{
+    @Id
+    private ObjectId id;
+    private String body;
+}
