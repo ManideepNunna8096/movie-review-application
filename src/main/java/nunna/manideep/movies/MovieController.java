d838624bd5e08c7d1e833513bb017f733b0df807
manideepnunna <nunnamanideep.study24@gmail.com>
2026-09-06 14:02:14 +0530

    Day 2: Add movie controller

---
+++ src/main/java/nunna/manideep/movies/MovieController.java
@@ -0,0 +1,16 @@
+package nunna.manideep.movies;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController{
+    @GetMapping
+    public ResponseEntity<String> allMovies(){
+        return new ResponseEntity<String>( "All Movies!",HttpStatus.OK); 
+    }
+}
