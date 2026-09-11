f07a449595a2a07d5c8b310cb5d4f0a7bbb7901a
manideepnunna <nunnamanideep.study24@gmail.com>
2026-09-11 09:05:54 +0530

    Day 6,7,8,9

--- src/main/java/nunna/manideep/movies/MovieController.java
+++ src/main/java/nunna/manideep/movies/MovieController.java
@@ -3,20 +3,35 @@ package nunna.manideep.movies;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.bson.types.ObjectId;
+
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-
 @RequestMapping("/api/v1/movies")
-public class MovieController{
+public class MovieController {
+
     @Autowired
     private MovieService movieService;
-    @GetMapping
 
-    public ResponseEntity<List<Movie>> getAllMovies(){
-        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK); 
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(
+                movieService.allMovies(),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(
+                movieService.singleMovie(imdbId),
+                HttpStatus.OK
+        );
     }
 }
--- src/main/java/nunna/manideep/movies/MovieService.java
+++ src/main/java/nunna/manideep/movies/MovieService.java
@@ -2,14 +2,22 @@ package nunna.manideep.movies;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.bson.types.ObjectId;
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
+public class MovieService {
 
-public class MovieService{
     @Autowired
     private MovieRepository movieRepositry;
-    public List<Movie> allMovies(){
+
+    public List<Movie> allMovies() {
         return movieRepositry.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepositry.findMovieByImdbId(imdbId);
     }
 }
